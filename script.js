2795055b06c6b06d41a2df726c28ce8d4bd8bd6c
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2024-01-27 13:09:30 +0100

    refaktor and fix bug with @media

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function scrollToElement(elementSelector, instance = 0) {
-    const elements = document.querySelectorAll(elementSelector);
-    if (elements.length > instance)
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
 }
 
+function addScrollListener(link, targetSelector, instance = 0) {
+  link.addEventListener("click", () => {
+    scrollToElement(targetSelector, instance);
+  });
+}
 
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-    
-});
-link2.addEventListener('click', () => {
-    scrollToElement('.header',1)
-});
-link3.addEventListener('click', () => {
-    scrollToElement('.column')
-});
+addScrollListener(link1, ".header");
+addScrollListener(link2, ".header", 1);
+addScrollListener(link3, ".column");
