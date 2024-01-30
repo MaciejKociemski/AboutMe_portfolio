4e371ed8932b93151abc99f0c04592a668885dff
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2024-01-30 12:01:52 +0100

    add portfolio skills, feedback, all will be changed , it only example

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ function addScrollListener(link, targetSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
 
 addScrollListener(link1, ".header");
 addScrollListener(link2, ".header", 1);
-addScrollListener(link3, ".column");
+addScrollListener(link3, ".header",3);
+addScrollListener(link4, ".header", 2);
+addScrollListener(link4, ".header", 4);
+
+
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
