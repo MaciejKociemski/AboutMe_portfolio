85f978052d77188ab991cf21d64a2a59b89f8593
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2024-02-01 10:52:52 +0100

    add ideas section

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 const link4 = document.getElementById("link4");
 const link5 = document.getElementById("link5");
+const link6 = document.getElementById("link6");
 
 addScrollListener(link1, ".header");
 addScrollListener(link2, ".header", 1);
-addScrollListener(link3, ".header", 3);
+addScrollListener(link3, ".header", 4);
 addScrollListener(link4, ".header", 2);
-addScrollListener(link4, ".header", 4);
+addScrollListener(link5, ".header", 3);
+addScrollListener(link6, ".header", 5);
 
 function openMenu() {
   const nav = document.querySelector("nav");
