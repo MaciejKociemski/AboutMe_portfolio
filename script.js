3b0f7e2c85b88a27fd310164633264f83878a6b0
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2024-02-02 15:16:12 +0100

    add arrow up and js function

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ function openMenu() {
   const nav = document.querySelector("nav");
   nav.classList.toggle("open");
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+  window.addEventListener("scroll", function () {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+  });
+
+  scrollToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
