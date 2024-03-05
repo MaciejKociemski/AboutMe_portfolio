63fb3f72b1501ab7d5f7bcee07cf7a4e270fbad6
Maciej Kociemski <maciejkociemski@MBP-Maciej.lan>
2024-03-05 20:48:25 +0100

    validator html, css

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,12 @@ let closeBtn = document.getElementsByClassName("close")[0];
 
 closeBtn.onclick = function () {
   modal.style.display = "none";
-  window.location.href = "index.html";
+  window.location.href = "../index.html";
 };
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    window.location.href = "./index.html";
+    window.location.href = "../index.html";
   }
 };
